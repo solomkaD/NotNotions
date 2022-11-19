7011259f2ed903a6472bf34a087eddb756b87fd5
Admin <i@3_012.ru>
2022-11-20 02:38:36 +0300

    password visibility button

--- app/src/main/java/com/example/notnotions/ExistElementActivity.kt
+++ app/src/main/java/com/example/notnotions/ExistElementActivity.kt
@@ -1,12 +1,18 @@
 package com.example.notnotions
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.InputType
+import android.text.method.PasswordTransformationMethod
+import android.view.View
 import android.widget.Button
 import android.widget.ImageButton
 import android.widget.TextView
 import androidx.appcompat.widget.Toolbar
+import androidx.core.view.isVisible
 import androidx.room.Room
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class ExistElementActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -29,13 +35,13 @@ class ExistElementActivity : AppCompatActivity() {
         val textPassword: TextView = findViewById(R.id.ElementPassword)
         val textSite: TextView = findViewById(R.id.ElementSite)
         val textNote: TextView = findViewById(R.id.ElementNote)
-        val buttonShowPassword: ImageButton = findViewById(R.id.buttonShowPassword)
 
         textLabel.text = element.label
         textUserName.text = element.username
         textPassword.text = element.password
         textSite.text = element.site
         textNote.text = element.note
+
     }
 
     override fun onSupportNavigateUp(): Boolean {
